a98c74c9158ca3e5b15bb19572e88d2a2526f46d
Sankalp Garg <sankalp@optexity.com>
2025-11-27 15:31:24 +0530

    added task to optexity

--- optexity/schema/task.py
+++ optexity/schema/task.py
@@ -1,12 +1,16 @@
+import base64
 import json
 import uuid
 from datetime import datetime
+from io import BytesIO
 from pathlib import Path
 from typing import Literal, Optional
 
+from PIL import Image
 from pydantic import BaseModel, Field, model_validator
 
 from optexity.schema.automation import Automation
+from optexity.schema.token_usage import TokenUsage
 
 
 class Task(BaseModel):
@@ -60,3 +64,74 @@ class Task(BaseModel):
         self.log_file_path.parent.mkdir(parents=True, exist_ok=True)
 
         return self
+
+
+class TaskCreateRequest(BaseModel):
+    task_id: str
+    recording_id: str
+    input_parameters: dict
+    unique_parameter_names: list[str]
+    created_at: datetime
+
+    @model_validator(mode="after")
+    def must_have_timezone(self):
+        if self.created_at.tzinfo is None:
+            raise ValueError("created_at must include timezone information")
+
+        for unique_parameter_name in self.unique_parameter_names:
+            if unique_parameter_name not in self.input_parameters:
+                raise ValueError(
+                    f"unique_parameter_name {unique_parameter_name} not found in input_parameters"
+                )
+        return self
+
+
+class TaskStartedRequest(BaseModel):
+    task_id: str
+    started_at: datetime
+
+    @model_validator(mode="after")
+    def must_have_timezone(self):
+        if self.started_at.tzinfo is None:
+            raise ValueError("started_at must include timezone information")
+        return self
+
+
+class TaskCompleteRequest(BaseModel):
+    task_id: str
+    child_process_id: int
+
+    status: Literal["success", "failed", "cancelled"]
+    error: str | None
+    completed_at: datetime
+    token_usage: TokenUsage
+
+    @model_validator(mode="after")
+    def must_have_timezone(self):
+        if self.completed_at.tzinfo is None:
+            raise ValueError("completed_at must include timezone information")
+        return self
+
+
+class TaskOutputDataRequest(BaseModel):
+    task_id: str
+    output_data: list[dict]
+    final_screenshot: str | None
+
+    @model_validator(mode="after")
+    def must_have_valid_final_screenshot(self):
+        if self.final_screenshot is not None and not self.is_valid_base64_image(
+            self.final_screenshot
+        ):
+            raise ValueError("final_screenshot must be a valid base64 encoded image")
+        return self
+
+    def is_valid_base64_image(self, data: str) -> bool:
+        try:
+            # Decode the base64 string
+            decoded = base64.b64decode(data, validate=True)
+            # Try to open it as an image
+            Image.open(BytesIO(decoded))
+            return True
+        except Exception as e:
+            return False
