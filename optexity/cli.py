db4fb23f3104acdd7b4b2c5473ad068e58106358
Sanjay Singh Rajpoot <sanjay@optexity.com>
2026-05-17 18:16:35 +0530

    fix: cli port fixed

--- optexity/cli.py
+++ optexity/cli.py
@@ -35,7 +35,9 @@ def install_browsers() -> None:
 def run_inference(args: argparse.Namespace) -> None:
     from optexity.inference.child_process import get_app_with_endpoints
 
-    app = get_app_with_endpoints(is_aws=args.is_aws, child_id=args.child_process_id)
+    app = get_app_with_endpoints(
+        is_aws=args.is_aws, child_id=args.child_process_id, port=args.port
+    )
     run(
         app,
         host=args.host,
